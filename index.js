c9c2f786d74dee693ed0b94a56c31988e1d66e9f
alittle1234 <aml.gr.rs@gmail.com>
2017-05-25 21:17:07 -0500

    git at c, post at d

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+// var bodyParser = require('body-parser')
+// app.use(bodyParser.json());       // to support JSON-encoded bodies
+// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  // extended: true
+// })); 
+// app.use(express.json());       // to support JSON-encoded bodies
+// app.use(express.urlencoded()); // to support URL-encoded bodies
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +19,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index2');
+});
+
+app.get('/c', function(request, response) {
+  response.send('pages/index2');
+});
+
+app.post('/d', function(req, res) {
+    res.send('pages/index2');
 });
 
 app.listen(app.get('port'), function() {
