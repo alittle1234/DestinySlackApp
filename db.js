a9d9598af3527410f780db7b7c44c428b2925a18
alittle1234 <aml.gr.rs@gmail.com>
2017-05-29 06:41:45 -0500

    moves code to db, test user select sql

---
+++ db.js
@@ -0,0 +1,103 @@
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+function getUsers(){
+	users = [];
+	results = getDBAction(=>{
+		client
+		.query('SELECT * FROM users;')
+		.on('row', function(row) {
+			console.log(JSON.stringify(row));
+			users[row.id] = row;
+	});});
+	
+	return users;
+}
+
+// id, name, img_url, destiny_name
+function storeUsers(users){
+	for(int i = 0; i < users.length; i++){
+		setDBAction(=>{
+		client
+			.query('SELECT id FROM FROM users where id='+users[i].id+';')
+			.on('row', function(row) {
+			   if(row && row.id == users[i].id){
+					updateUser(users[i]);
+			   }else{
+					insertUser(users[i]);
+			   }
+			});
+		});
+	}
+}
+
+function insertUser(user){
+	setDBAction(=>{
+	client.query('INSERT INTO users(id, name, img_url, destiny_name) ' +
+		'values($1, $2, $3, $4)',
+		[user.id, user.name, user.img_url, user.destiny_name]);
+	});
+}
+
+function updateUser(user){
+	setDBAction(=>{
+	client.query('UPDATE items SET name=($1), img_url=($2), destiny_name=($3) WHERE id=($4)',
+		[user.name, user.img_url, user.destiny_name, user.id]);
+	});
+}
+
+function getDBAction(queryMethod){
+	pg.connect(connectionString, (err, client, done) => {
+		const results = [];
+		// Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+		}
+		
+		// SQL Query > Select Data
+		const query = queryMethod();
+		// Stream results back one row at a time
+		query.on('row', (row) => {
+			results.push(row);
+		});
+		
+		// After all data is returned, close connection and return results
+		query.on('end', () => {
+			done();
+			return results;
+		});
+	});
+}
+
+function setDBAction(queryMethod){
+	pg.connect(connectionString, (err, client, done) => {
+		// Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+		}
+		
+		// SQL Query > Select Data
+		const query = queryMethod();
+		
+		// After all data is returned, close connection and return results
+		query.on('end', () => {
+			done();
+			return;
+		});
+	});
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+var db = require('./db');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,22 +14,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
 
 app.get('/', function(req, res) {
   res.render('pages/index2');
+});
+
+app.get('/users/', function(req, res) {
+	res.send(db.getUsers());
 });
 
 app.listen(app.get('port'), function() {
