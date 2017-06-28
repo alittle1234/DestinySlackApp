7af5ebb936f339e35247146c9207a2ba0774102b
alittle1234 <aml.gr.rs@gmail.com>
2017-06-27 20:39:24 -0500

    adds more data to message class

--- message.js
+++ message.js
@@ -4,13 +4,66 @@ var message = {
 	originalRu: "http...",
 	dateAdded: "1234",
 	dateModified: "1234",
-	poster: "uid",
+	userId: "uid",
 	data: "other json"
+}
+
+class Message{
+	constructor(timestamp, responseUrl, dateAdded, dateModified, userId, data){
+		this.timestamp = timestamp;
+		this.responseUrl = responseUrl;
+		this.dateAdded = dateAdded;
+		this.dateModified = dateModified;
+		this.userId = userId;
+		this.data = data;
+		
+		this.messageData = Message.deserializeData(this.data);
+	}
+	static deserializeData(dataStr){
+		var j = JSON.parse(dataStr);
+		return new MessageData(j.join, j.activity, j.time, j.timeZone);
+	}
+	static serializeData(mesgData){
+		var j = {
+			join: mesgData.join,
+			activity: mesgData.activity,
+			time: mesgData.time,
+			timeZone: mesgData.timeZone,
+		}
+		return JSON.stringify(j);
+	}
 }
 var origMesCache = {
 	
 }
 
+var messageData={
+	join: {
+		hasJoin: true, 
+		hasNo: true, 
+		hasMaybe: true, 
+		hasStandby: true, 
+		hasYesLimit: true,
+		joins: ["Uid1"],
+		yess: ["Uid1"],
+		nos: ["Uid1"],
+		maybes: ["Uid1"],
+		yesLimit: 6,
+	},
+	activity: ["Trials", "Raid"...],
+	date: 12345,
+	localTz: "Central",
+	
+}
+
+class MessageData{
+	constructor(join, activity, time, timeZone){
+		this.join = join;
+		this.activity = activity;
+		this.time = time;
+		this.timeZone = timeZone;
+	}
+}
 // origMesCache[message.originalTs] = message;
 // update cache
 
